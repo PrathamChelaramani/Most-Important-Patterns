453c5c3a3622aead2446c18e463783a759704686
chelaramanipratham <100186226+PrathamChelaramani@users.noreply.github.com>
2024-06-04 11:44:47 +0530

    Add files via upload

---
+++ Pattern_14.py
@@ -0,0 +1,13 @@
+n=4
+# char = ascii("A")
+for i in range(n):
+    for j in range(i+1):
+        print(chr(65+j), end=" ")
+    print()
+
+
+
+# A 
+# A B
+# A B C
+# A B C D
---
+++ Pattern_15.py
@@ -0,0 +1,10 @@
+for i in range(4):
+    for j in range(4-i):
+        print(chr(65+j), end=" ")
+    print()
+
+
+# A B C D
+# A B C
+# A B
+# A
